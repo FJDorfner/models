556373f91e574cc60ddec20ff201067241496c5d
FJDorfner <felix.dorfner@charite.de>
2024-06-27 22:00:21 +0000

    Added the model code and default config for deepspa

--- models/deepspa/utils/DeepSpARunner.py
+++ models/deepspa/utils/DeepSpARunner.py
@@ -11,110 +11,99 @@ Email:  felix.dorfner@charite.de
 """
 import torch.cuda
 from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput, Meta
-
 from typing import Dict
-import json
-from pathlib import Path
-
+import os
 import torch
+import Model_Class
+import Model_Seg
+import SimpleITK as sitk
+import utils
+from numpy import uint8, rot90, fliplr
+from monai.transforms import Rotate90
+from segdb.classes.Segment import Segment, Triplet
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-@ValueOutput.Name('lncancerprob')
+@ValueOutput.Name('raxSpAprob')
 @ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
-@ValueOutput.Label('Lung Nodule cancer probability score.')
+@ValueOutput.Label('radiographic axSpA probability score')
 @ValueOutput.Type(float)
-@ValueOutput.Description('The predicted cancer probability score for a single lung nodule detected by the algorithm')
-class LNCancerProb(ValueOutput):
+@ValueOutput.Description('The predicted probability score for the patient having radiographic axSpA by the algorithm')
+class raxSpAprob(ValueOutput):
+   pass
+
+@ValueOutput.Name('nraxSpAprob')
+@ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
+@ValueOutput.Label('no radiographic axSpA probability score')
+@ValueOutput.Type(float)
+@ValueOutput.Description('The predicted probability score for the patient not having r-axSpA by the algorithm')
+class nraxSpAprob(ValueOutput):
+   pass
+
+@ValueOutput.Name('binarized_pred')
+@ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
+@ValueOutput.Label('Binarized prediction for the presence of r-axSpA, using the cut-off 0.59')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Binarized prediction for the presence of r-axSpA, using the cut-off 0.59')
+class binarized_pred(ValueOutput):
    pass
 
 
-@ValueOutput.Name('clcancerprob')
-@ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
-@ValueOutput.Label('Case level cancer probability score.')
-@ValueOutput.Type(float)
-@ValueOutput.Description('The predicted cancer probability score for the whole case')
-class CLCancerProb(ValueOutput):
-    pass
+# register custom segmentation before class definition
+Triplet.register("PELVIS", code="12921003", meaning="Pelvis", scheme_designator="SCT", overwrite=True)
+Segment.register("LEFT_PELVIS", name="Left Pelvis", category="C_BODY_STRUCTURE", type="PELVIS", modifier="M_LEFT")
+Segment.register("RIGHT_PELVIS", name="Right Pelvis", category="C_BODY_STRUCTURE", type="PELVIS", modifier="M_RIGHT")
 
-
-# This method cleans the raw results from the grt123 algorithm output and only keeps the relevant details
-def cleanup_json_report(data: Dict):
-    for key in ["trainingset1", "trainingset2"]:
-        del data["lungcad"][key]
-    for key in ["patientuid", "studyuid"]:
-        del data["imageinfo"][key]
-    data["findings"] = [
-        dict(
-            id=f["id"],
-            x=f["x"],
-            y=f["y"],
-            z=f["z"],
-            probability=f["probability"],
-            cancerprobability=f["cancerprobability"]
-        )
-        for f in data["findings"]
-    ]
-
-
-@IO.Config('n_preprocessing_workers', int, 6, the="number of preprocessing workers to use for the grt123 lung mask preprocessor")
-class LungCancerClassifierRunner(Module):
-
-    n_preprocessing_workers: int
+class DeepSpARunner(Module):
 
     @IO.Instance()
-    @IO.Input('in_data', 'mha:mod=ct', the='input ct scan')
-    @IO.Output('out_data', 'grt123_lung_cancer_findings.json', 'json:model=grt123LungCancerClassification', data='in_data', the='predicted nodules and lung cancer findings of the lung lobe')
-    @IO.OutputData('clcancerprob', CLCancerProb, the='Case level probability score')
-    @IO.OutputDatas('lncancerprobs', LNCancerProb, the='Individual lung nodule probability scores')
-    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData, clcancerprob: CLCancerProb, lncancerprobs: LNCancerProb) -> None:
-        # create temporary directories for the preprocessed data and the cropped bounding boxes
-        tmp_path = Path(self.config.data.requestTempDir('grt123'))
-        tmp_output_bbox_dir = tmp_path / "bbox"
-        tmp_output_prep_dir = tmp_path / "prep"
-        tmp_output_bbox_dir.mkdir(exist_ok=True, parents=True)
-        tmp_output_prep_dir.mkdir(exist_ok=True, parents=True)
+    @IO.Input('in_data', 'dicom', the='input xray scan')
+    @IO.Output('out_data', 'segmentation_overlay.png', 'png:mod=seg:model=DeepSpA:roi=SACRUM,LEFT_PELVIS,RIGHT_PELVIS', the="Output segmentation mask containing all labels, overlayed on the input image")
+    @IO.Output('out_data_box', 'boxed_image.png', 'png:mod=box:model=DeepSpA:roi=SACRUM,LEFT_PELVIS,RIGHT_PELVIS', the="The input image, with the bounding box applied. This image was used as the input to the classification model.")
+    @IO.Output('out_data_gradcam', 'gradcam_image.png', 'png:mod=gradcam:model=DeepSpA:roi=SACRUM,LEFT_PELVIS,RIGHT_PELVIS', the="Grad-CAM visualization of the classification model, which runs its inference on the bounding box cropped image.")
+    @IO.OutputData('raxSpAprob', raxSpAprob)
+    @IO.OutputData('nraxSpAprob', nraxSpAprob)
+    @IO.OutputData('binarized_pred', binarized_pred)
+    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData, raxSpAprob: raxSpAprob, nraxSpAprob: nraxSpAprob, binarized_pred: binarized_pred, out_data_box: InstanceData, out_data_gradcam: InstanceData) -> None:
 
-        # determine the number of GPUs we can use
-        if torch.cuda.is_available():
-            self.log("Running with a GPU", "NOTICE")
-            n_gpu = 1
-        else:
-            self.log("Running on the CPU, might be slow...", "NOTICE")
-            n_gpu = 0
+      device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   
+      im_series = os.listdir(in_data.abspath)[0]
+      in_path = os.path.join(in_data.abspath, im_series)
 
-        # Import the main module for the grt123 algorithm, which must be used for running the classification
-        import main
+      image_mask = Model_Seg.load_and_segment_image(in_path, device)
 
-        # apply grt123 algorithm
-        results = main.main(
-            skip_detect=False,
-            skip_preprocessing=False,
-            datapath=str(Path(in_data.abspath).parent),
-            outputdir=str(tmp_path),
-            output_bbox_dir=str(tmp_output_bbox_dir),
-            output_prep_dir=str(tmp_output_prep_dir),
-            n_gpu=n_gpu,
-            n_worker_preprocessing=self.n_preprocessing_workers,
-            data_filter=r".*.mha"
-        )
 
-        # retrieve classification results
-        assert len(results) > 0, "LungCancerClassifierRunner - Always expects at least one output report"
-        results_dict = results[0].to_json()
-        cleanup_json_report(results_dict)
 
-        # export to JSON (original json file)
-        self.log(f"Writing classification results to {out_data.abspath}", "NOTICE")
-        with open(out_data.abspath, "w") as f:
-            json.dump(results_dict, f, indent=4)
+      overlay_image_np, original_image_np = utils.overlay_mask(in_path, image_mask)
+      overlay_image_np = rot90(overlay_image_np, k=3)
+      overlay_image_np = fliplr(overlay_image_np)
+      plt.imsave(out_data.abspath, np.ascontiguousarray(overlay_image_np), cmap='gray')
 
-        # set output value for case level cancer probability
-        clcancerprob.value = results_dict["cancerinfo"]["casecancerprobability"]
+      image_mask_im = sitk.GetImageFromArray(image_mask[None, :, :].astype(uint8))
+      image_im = sitk.GetImageFromArray(original_image_np[None, :, :].astype(uint8))
+      cropped_boxed_im, _ = utils.mask_and_crop(image_im, image_mask_im)
 
-        # set output values for nodule level cancer probabilities
-        for finding in results_dict["findings"]:
-            nodule_cancer_prob = LNCancerProb()
-            nodule_cancer_prob.meta = Meta(id=finding['id'], x=finding['x'], y=finding['y'], z=finding['z'], )
-            nodule_cancer_prob.description += f" (for nodule {finding['id']} at location ({finding['x']}, {finding['y']}, {finding['z']}))"
-            nodule_cancer_prob.value = finding["cancerprobability"]
-            lncancerprobs.add(nodule_cancer_prob)
+      cropped_boxed_array = sitk.GetArrayFromImage(cropped_boxed_im)
+      cropped_boxed_tensor = torch.Tensor(cropped_boxed_array)
+      rotate = Rotate90(spatial_axes=(0, 1), k=3)
+
+      cropped_boxed_tensor = rotate(cropped_boxed_tensor)
+      cropped_boxed_array_disp = cropped_boxed_tensor.numpy().squeeze().astype(uint8)
+      prediction, image_transformed = Model_Class.load_and_classify_image(cropped_boxed_tensor, device)
+      plt.imsave(out_data_box.abspath, cropped_boxed_array_disp, cmap='gray')
+
+
+      gradcam = Model_Class.make_GradCAM(image_transformed, device)
+      plt.imsave(out_data_gradcam.abspath, gradcam, cmap='jet')
+
+      nr_axSpA_prob = float(prediction[0].item())
+      r_axSpA_prob = float(prediction[1].item())
+
+
+      raxSpAprob.value = r_axSpA_prob
+      nraxSpAprob.value = nr_axSpA_prob
+
+      # Prediction gets binarized based on the predetermined threshold
+      binarized_pred.value = float(r_axSpA_prob > 0.59)
